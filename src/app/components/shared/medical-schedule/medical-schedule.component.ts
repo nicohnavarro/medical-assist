7553f96cfc7a6345aa6e6bc4710bff5cc6f789b8
nicohnavarro <nicohnavarro@gmail.com>
2021-07-07 02:02:48 -0300

    :hammer: add medical schedule

--- src/app/components/shared/medical-schedule/medical-schedule.component.ts
+++ src/app/components/shared/medical-schedule/medical-schedule.component.ts
@@ -1,6 +1,4 @@
 import { WorkDaysService } from './../../../services/work-days.service';
-import { AuthService } from './../../../services/auth.service';
-import { Days } from '../../../utils/days.enum';
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-medical-schedule',
@@ -9,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicalScheduleComponent implements OnInit {
 
-  scheduleExample:any[];
+  scheduleExample:WorkSchedule[];
   active:boolean = false;
   doctorInfo;
   daysWork;
+  daysEnables: WorkDay[]=[];
 
   constructor(private workDaysSvc:WorkDaysService) {
     let idDoc = localStorage.getItem('uid');
@@ -27,24 +26,57 @@ export class MedicalScheduleComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getDays();
     this.getSchedule();
   }
 
-  getSchedule(){
+  getDays(){
+    this.daysEnables = [
+      {name:'LUNES',active:false,schedule:this.getSchedule()},
+      {name:'MARTES',active:false,schedule:this.getSchedule()},
+      {name:'MIERCOLES',active:false,schedule:this.getSchedule()},
+      {name:'JUEVES',active:false,schedule:this.getSchedule()},
+      {name:'VIERNES',active:false,schedule:this.getSchedule()},
+      {name:'SABADO',active:false,schedule:this.getSchedule()},
+    ]
+  }
+
+  getSchedule():WorkSchedule[]{
+    const schedule = [];
     let exampleDate = new Date('11/11/2020 8:00:00');
     let first = exampleDate.getHours()+':00';
     this.scheduleExample.push({'hour':first,'active':true});
+    schedule.push({'hour':first,'active':true});
     while(exampleDate.getHours()<19){
       exampleDate.setMinutes( exampleDate.getMinutes() + 30 );
       let minutes = exampleDate.getMinutes() === 0 ? '00':'30'
       let hour = exampleDate.getHours()+':'+minutes;
       this.scheduleExample.push({hour,'active':false});
+      schedule.push({hour,'active':false});
     }
+    return schedule;
   }
+
 
   selectTime(hour){
     hour.active ? hour.active= false: hour.active = true;
     console.log(this.doctorInfo);
     console.log(this.daysWork);
   }
+  selectDay(hour){
+    hour.active ? hour.active= false: hour.active = true;
+    console.log(this.doctorInfo);
+    console.log(this.daysWork);
+  }
+}
+
+export interface WorkDay{
+  name:string,
+  active:boolean,
+  schedule?:any[]
+}
+
+export interface WorkSchedule{
+  hour:string,
+  active:boolean,
 }
