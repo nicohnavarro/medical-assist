1aabdf59a080c96297e45cc983cfa6a6c485f962
nicohnavarro <nicohnavarro@gmail.com>
2021-07-13 23:48:01 -0300

    :bug: fix pdf generator

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -1,12 +1,15 @@
 import { UserService } from './../../services/user.service';
 import { MedicalSpecialtiesService } from './../../services/medical-specialties.service';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import jsPDF from 'jspdf';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-import htmlToPdfmake from 'html-to-pdfmake';
 import html2canvas from 'html2canvas';
+
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import { UserOptions } from 'jspdf-autotable';
+
+interface jsPDFWithPlugin extends jsPDF.jsPDF {
+  autoTable: (options: UserOptions) => jsPDF.jsPDF;
+}
 
 @Component({
   selector: 'app-statistics',
@@ -16,9 +19,9 @@ import html2canvas from 'html2canvas';
 export class StatisticsComponent implements OnInit {
   @ViewChild('pdfTable') pdfTable: ElementRef;
 
-  barData:any[];
-  pieData:any[];
-  constructor(private medicalSpecialtySvc:MedicalSpecialtiesService,private userSvc:UserService) { 
+  barData: any[];
+  pieData: any[];
+  constructor(private medicalSpecialtySvc: MedicalSpecialtiesService, private userSvc: UserService) {
     this.medicalSpecialtySvc.getSpecialties().subscribe((specialties) => {
       this.barData = specialties.map((specialty) => { return ({ 'name': specialty.name, 'value': specialty?.shifts }) })
       this.pieData = specialties.map((specialty) => { return ({ 'name': specialty.name, 'value': specialty?.doctors || 0 }) })
@@ -29,21 +32,36 @@ export class StatisticsComponent implements OnInit {
   }
 
   public downloadAsPDF() {
-    const DATA:any = document.getElementById('pdfTable');
-    const doc = new jsPDF('p','pt','a4');
+    const DATA: any = document.getElementById('heatMap');
+    const doc = new jsPDF.jsPDF('p', 'pt', 'a4') as jsPDFWithPlugin;
+
+    doc.autoTable({
+      head: [['Name', 'Email', 'Country']],
+      body: [
+        ['David', 'david@example.com', 'Sweden'],
+        ['Castille', 'castille@example.com', 'Norway']
+      ]
+    });
+
     const options = {
-      background:'white',
-      scale:3,
+      background: 'white',
+      scale: 1,
     }
-    html2canvas(DATA,options)
-      .then((canvas)=>{
+    html2canvas(DATA, options)
+      .then((canvas) => {
         const img = canvas.toDataURL('image/PNG');
 
         const bufferX = 15;
         const bufferY = 15;
         const imgProps = (doc as any).getImageProperties(img);
-        const pdfWidth = doc.internal.pageSize.getWidth() -2 * bufferX;
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
         const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+        const columns = [
+          { title: "Name", dataKey: "name" },
+          { title: "Quantity", dataKey: "value" }
+        ];
+
         doc.addImage(
           img,
           'PNG',
@@ -56,22 +74,20 @@ export class StatisticsComponent implements OnInit {
         );
         return doc;
       })
-      .then((docResult)=>{
+      .then((docResult) => {
+        doc.autoTable({
+          headStyles: {fillColor: [71, 27, 227]},
+          margin: { top: 10 },
+          bodyStyles:{cellPadding:{top: 10, right: 20, bottom: 10, left: 10},font:'helvetica'},
+          body: this.barData,
+          columns: [
+            { header: 'Name', dataKey: 'name' },
+            { header: 'Quantity', dataKey: 'value' },
+          ],
+        });
         docResult.save('pruebanico.pdf');
       });
 
-  //   const pdfTable = this.pdfTable.nativeElement;
-   
-  //   var html = htmlToPdfmake(pdfTable.innerHTML);
-  //   console.log(html[1].stack[1]);
-  //   const documentDefinition = {
-  //     pageSize: {
-  //     width: 1000,
-  //     height: 200
-  // },
-  // content: html[1],width: 1000,height:100};
-  //   pdfMake.createPdf(documentDefinition).open(); 
-     
   }
 
 }
